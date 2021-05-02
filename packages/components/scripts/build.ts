c44d84c6aa96252a6674db3f5a0e321e10f7d99b
Allen Jacobson <allensy89@gmail.com>
2021-05-02 16:43:08 +0300

    Modified the build script

--- packages/components/scripts/build.ts
+++ packages/components/scripts/build.ts
@@ -1,5 +1,8 @@
 require('esbuild').build({
     entryPoints: ['./src/types.ts'],
     bundle: true,
+    minify: true,
+    sourcemap: true,
+    target: ['chrome58', 'firefox57', 'safari11'],
     outfile: 'dist/bundled.js',
 }).catch(() => process.exit(1))
